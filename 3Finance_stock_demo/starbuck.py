267c7e4f3901414f1bb2b6103bc42db52c4ea1fc
ariclavel <a01706629@itesm.mx>
2023-12-04 19:11:16 +0100

    changes

---
+++ 3Finance_stock_demo/starbuck.py
@@ -0,0 +1,146 @@
+# Import required libraries
+import requests
+from bs4 import BeautifulSoup
+import csv
+import time
+import random
+import hashlib
+
+# Extract review information
+reviews_sb = []
+h = []
+def save_to_csv(h):
+    try:
+        with open('reviews_hw.csv', 'w', encoding='utf-16', newline='') as csvfile:
+            fieldnames = h
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='\t')
+            if csvfile.tell() == 0:
+                writer.writeheader()
+            for index, review in enumerate(reviews_sb, start=1): 
+                print(review)
+                writer.writerow(review)
+            
+    except ValueError:
+        print(ValueError.text())
+
+
+#Input url
+trustpilot_urls = [
+    "https://fr.trustpilot.com/review/beautechoix.com"
+]
+
+
+def get_reviews(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser",from_encoding='utf-16')
+        review_elements = soup.find_all("div", class_="styles_reviewCardInner__EwDq2")
+        reviews = []
+        for review in review_elements:
+            # Input functions, tag and class
+            user = review.find(
+                "span",
+                class_="typography_heading-xxs__QKBS8",
+            ).text
+            user_id = int(hashlib.sha256(user.encode("utf-16")).hexdigest(), 16) % (10 ** 8)
+            
+            heading = review.find(
+                "h2",
+                class_="typography_heading-s__f7029 typography_appearance-default__AAY17",
+            )
+            if not heading:
+                heading=""
+            else:
+                heading = heading.text
+            message = review.find(
+                "p",
+                class_="typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn",
+            )
+            if not message:
+                message=""
+            else:
+                message = message.text
+
+            date = review.find(
+                "time"
+            )
+            if not date:
+                date=""
+            else:
+                date = date.text
+            rating = review.find(
+                "div",
+                class_='star-rating_starRating__4rrcf star-rating_medium__iN6Ty'
+            ).img['alt']
+            date_review = review.find(
+                "p",
+                class_ ="typography_body-m__xgxZ_ typography_appearance-default__AAY17" 
+            ).text
+            date_review = date_review.split(":")[1]
+            reviews.append(
+                {
+                    "User_id": user_id,
+                    "Heading": heading,
+                    "Message": message,
+                    "Date": date,
+                    "Rating": rating,
+                    "Data_review": date_review
+                }
+            )
+            reviews_sb.append(
+                {
+                    "User_id": user_id,
+                    "Heading": heading,
+                    "Message": message,
+                    "Date": date,
+                    "Rating": rating,
+                    "Data_review": date_review
+                }
+            )
+
+            
+        return reviews
+
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to retrieve page {url}. Exception: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    for url in trustpilot_urls:
+        base_url = "?"
+        num_pages = 17 
+        #179
+        for page in range(1, num_pages + 1):
+            page_url = f"{url}?page={page}"
+            print(f"\nScraping reviews for {page_url}")
+            delay_seconds = random.uniform(0.1, 1)
+            time.sleep(delay_seconds)
+
+            reviews = get_reviews(page_url)
+            #save_to_csv(reviews,h)
+            # Input reviews
+            if reviews:
+                #reviews_sb.append(reviews)
+                print(f"\nlength {len(reviews)}:")
+                for index, review in enumerate( reviews, start=1):
+                    print(f"\nReview #{index}:")
+                    print(f"Userid: {review['User_id']}")
+                    print(f"Heading: {review['Heading']}")
+                    print(f"Message: {review['Message']}")
+                    print(f"Date: {review['Date']}")
+                    print(f"Rating: {review['Rating']}")
+                    print(f"Data review: {review['Data_review']}")
+                   
+            else:
+                print(f"No reviews found for {url}")
+
+        
+        for key in reviews_sb[0].keys():
+            h.append(key)
+        print(h)
+        save_to_csv(h)
+        
+        
+        
